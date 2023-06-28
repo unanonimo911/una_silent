3978b119078cf14fe92d9a5fc124e6a1372b4753
unanonimo911 <137927338+unanonimo911@users.noreply.github.com>
2023-06-28 17:44:04 +0200

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,4 +1,4 @@
 --UNA ANTISILENT--
 Config = {}
-Config.Weebhook = "https://discord.com/api/webhooks/1083101496134078556/r3Ed8LECV4eKroYJgITgOGGE3JZ03piDPYZsOx2eIn_aIARMauiJq9FMe0FnxvvKyCTw"
+Config.Weebhook = ""
 Config.Msg = "SE TE HA DETECTADO SILENT AIM"
