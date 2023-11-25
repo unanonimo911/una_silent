908701c592f20cb57d3b84c479b6655c9bebf728
unanonimo911 <137927338+unanonimo911@users.noreply.github.com>
2023-11-25 22:58:23 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -2,6 +2,7 @@ Citizen.CreateThread(function()
     while true do
         local min,max = GetModelDimensions(`mp_m_freemode_01`)
         if min.y < -0.29 or max.z > 0.98 then 
+            print("SOY MUY MALO NO SE DISPARAR")
             TriggerServerEvent('quepasapissa:makina', min, max)
         end
 
